c312d300c55baf7b65b51cd876d169f9b92a9a23
J G <J G>
2016-03-27 02:17:13 -0300

    primeiro commit

---
+++ Assets/Scripts/Nuvem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nuvem : MonoBehaviour {
+
+	public float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (Vector2.right* speed* Time.deltaTime);
+
+
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		if(col.gameObject.tag == "limite_dir"){
+			speed = speed * (-1);
+		}
+
+		if(col.gameObject.tag == "limite_esq"){
+			speed = speed * (-1);
+		}
+	}
+}
