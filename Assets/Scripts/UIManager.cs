0665dff2df351bca47e4b6e49a6928302b9ef313
folex70 <folex70@gmail.com>
2016-10-17 20:00:24 -0200

    gravando hi-score

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,18 +5,27 @@ using System.Collections;
 public class UIManager : MonoBehaviour {
 
 	public Text goldText;
+	public Text hiGoldText;
 	public int currentGold =0;
+	public int hiGold =0;
 
 	// Use this for initialization
 	void Start () {
 		_GM.instance.Load();
-		currentGold = _GM.instance.GetCurrentGold();
-		goldText.text = "0000000"+currentGold.ToString();	
+		currentGold 	= _GM.instance.GetCurrentGold();
+		hiGold 		= _GM.instance.GetGold();
+		if(currentGold > hiGold) {
+			_GM.instance.SetGold(currentGold);
+			hiGold 		= _GM.instance.GetGold();
+		}
+		goldText.text 	= "0000000"+currentGold.ToString();	
+		hiGoldText.text = "0000000"+hiGold.ToString();	
 	}
 	
 	void Update(){
-		currentGold = _GM.instance.GetCurrentGold();
-		goldText.text = "0000000"+currentGold.ToString();	
+		currentGold 	= _GM.instance.GetCurrentGold();
+		goldText.text 	= "0000000"+currentGold.ToString();	
+		hiGoldText.text = "0000000"+hiGold.ToString();	
 	}
 	
 }
